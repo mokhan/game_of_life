3100c4b4c95f42a2e9e73903eb61ca92d6474270
mo khan <mo@mokhan.ca>
2014-06-22 21:40:02 -0600

    clear the screen before next generation.

--- lib/gol/printer.rb
+++ lib/gol/printer.rb
@@ -4,9 +4,17 @@ class Printer
   end
 
   def display(world)
+    clear
     world.each_with_index do |cell, index|
       print cell.alive? ? "X" : " "
       puts if (index+1) % @max_x == 0
     end
+    sleep 1
+  end
+
+  private
+
+  def clear
+    puts "\e[H\e[2J"
   end
 end
